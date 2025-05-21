fc4e9ec92fb24e5852c6960ea87d780934123373
Gudipati Vishnu Vardhan Reddy <70902772+vishan01@users.noreply.github.com>
2025-05-21 22:15:45 +0530

    Add user schema fields for username, fullname, email, password, avatar, and coverImage

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,6 +1,40 @@
 import mongoose from "mongoose";
 
 const UserSchema=mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        index:true,
+    },
+    fullname:{
+        type:String,
+        required:true,
+        trim:true,
+        lowercase:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        index:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true,
+    }, 
+    avatar:{
+        type:String,
+        default:"https://i.pinimg.com/736x/8e/77/69/8e77695f76f0b51f0d82c1060d858e9f.jpg",
+    },
+    coverImage:{
+        type:String,
+    },
     
 })
 
